b3e36a480b515e196a956eceda3dab2fce114772
Alessandro Paghera <apaghera@2-H-1.42heilbronn.de>
2023-10-17 16:04:30 +0200

    add float check

--- cpp06/ex00/Convert.cpp
+++ cpp06/ex00/Convert.cpp
@@ -45,6 +45,61 @@ std::string ScalarConverter::getInput()
 {
 	return (_input);
 }
+
+static int countDot(const std::string &format)
+{
+	int dotCount;
+
+	dotCount = 0;
+
+	for (size_t i = 0; i < format.length(); i++)
+	{
+		if (!isdigit(format[0]))
+			return (false);
+		if (!isdigit(format[i]))
+		{
+			if (format[i] == '.' && !isdigit(format[i + 1]))
+				return (false);
+			else if (format[i] == '.')
+				dotCount++;
+		}
+		if (dotCount > 1)
+			return (false);
+	}
+	return (dotCount);
+}
+
+static bool isFloat(const std::string &input)
+{
+	char 		*endptr;
+	std::string validFormat;
+	int 		dotCount;
+
+	dotCount = 0;
+	if (input.length() > 0 && input[input.length() - 1 ] == 'f')
+	{
+		validFormat = input.substr(0, input.length() - 1);
+		dotCount = countDot(validFormat);
+		strtof(validFormat.c_str(), &endptr);
+		if (dotCount == 1)
+			return (*endptr == '\0');
+	}
+	else if (input.length() > 0)
+	{
+		strtof(input.c_str(), &endptr);
+		dotCount = countDot(input);
+		if (dotCount == 1)
+			return (*endptr == '\0');
+	}
+	return (false);
+}
+
+void ScalarConverter::convert(std::string &input)
+{
+	if (!isFloat(input))
+		std::cout << "Is not a float" << std::endl;
+	else
+		std::cout <<"Is a float" << std::endl;
+}
+
 ScalarConverter::~ScalarConverter() {}
-
-
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,8 +1,14 @@
 #include "Convert.hpp"
 
-
-int main(void)
+int main(int argc, char **argv)
 {
-
+	if (argc == 1)
+		std::cout << "Argument 1 expected 2" << std::endl;
+	else
+	{
+		std::string	input(argv[1]);
+		ScalarConverter test(input);
+		test.convert(input);
+	}
 	return (0);
 }
