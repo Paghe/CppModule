d779f42117e820f10558c6309cf6b6b78f6a7768
Alessandro Paghera <apaghera@3-G-6.42heilbronn.de>
2023-10-19 15:49:06 +0200

    watch line 54 in utils.cpp

--- cpp06/ex00/utils.cpp
+++ cpp06/ex00/utils.cpp
@@ -4,33 +4,34 @@ typedef void (*FunctionArray)(const void *format, std::string token);
 
 typedef void (*CastArray)(const void *format);
 
-void	castDouble(const void* format)
+void	castDoubleAscii(const void* format)
 {
 	const double value = *static_cast<const double*>(format);
-	char ascii = static_cast<char>(value);
-	std::cout << ascii << std::endl;
+   	char ascii = static_cast<char>(value);
+	std::cout << "Problem -> " << ascii << std::endl;
 }
 
-void	castFloat(const void* format)
+void	castFloatAscii(const void* format)
 {
 	const float value = *static_cast<const float*>(format);
 	char ascii = static_cast<char>(value);
 	std::cout << ascii << std::endl;
 }
 
-void	castInt(const void* format)
+void	castIntAscii(const void* format)
 {
 	const int value = *static_cast<const int*>(format);
 	char ascii = static_cast<char>(value);
+	std::cout << ascii << std::endl;
 }
 
 void	printChar(const void* format, std::string token)
 {
-	CastArray printCast[3] = {&castDouble, &castFloat, &castInt};
+	CastArray printCast[3] = {&castDoubleAscii, &castFloatAscii, &castIntAscii};
 	std::string castMatch[3] = {"double", "float", "int"};
 	for (int i = 0; i < 3; i++)
 	{
-		if (token == castMatch[i])
+		if (castMatch[i] == token)
 		{
 			printCast[i](format);
 			return ;
@@ -50,13 +51,13 @@ void	printFloat(const void* format, std::string token)
 void	printDouble(const void* format, std::string token)
 {
 	std::stringstream stream;
-	float value = *static_cast<const double*>(format);
+	double value = *static_cast<const double*>(format); // change the type bc is not working from double to float -> <const float>
 	stream << std::fixed << std::setprecision(1) << value;
 	std::cout << stream.str() << std::endl;
 	printChar(format, token);
 }
 
-void printOneDecimal(const void* format, std::string token)
+void printOneDecimal(const void* format, std::string token) // probably change name function
 {
 	std::string const matchArray[FORMAT_ARRAY] = {"float", "double", "char"};
 	FunctionArray formatArray[FORMAT_ARRAY] = {&printFloat, &printDouble, &printChar};
@@ -94,7 +95,7 @@ void convertFloat(std::string const &input)
 	validFormat = input.substr(0, input.length() - 1);
 	float check = strtof(validFormat.c_str(), &endptr);
 	printOneDecimal(&check, "float");
-	printOneDecimal(&check, "char");
+	printOneDecimal(&check, "double");
 }
 
 void convertDouble(std::string const &input)
