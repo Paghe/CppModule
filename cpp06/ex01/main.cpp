b0a6607fad867124bf041abad7a6ac260032f8c0
Alessandro Paghera <apaghera@2-H-1.42heilbronn.de>
2023-11-04 14:01:24 +0100

    star ex2

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,17 +3,17 @@
 
 int	main(void)
 {
-	Data DataSerialezer;
+	Data DataSerializer;
 
-	DataSerialezer.value = 42;
-	uintptr_t ptr = Serializer::serialize(&DataSerialezer);
+	DataSerializer.value = 42;
+	uintptr_t ptr = Serializer::serialize(&DataSerializer);
 	Data *DataCopy = Serializer::deserialize(ptr);
 	uintptr_t ptrCopy = Serializer::serialize(DataCopy);
 	std::cout << "Numeric address rep ---> " << ptr << std::endl;
 	std::cout << "Numeric address rep cpy ---> " << ptrCopy << std::endl;
-	std::cout << "Address struct ---> " << &DataSerialezer << std::endl;
+	std::cout << "Address struct ---> " << &DataSerializer << std::endl;
 	std::cout << "Address struct cpy ---> " << DataCopy << std::endl;
-	std::cout << "Original value ---> " << DataSerialezer.value << std::endl;
+	std::cout << "Original value ---> " << DataSerializer.value << std::endl;
 	std::cout << "Value cpy ---> " << DataCopy->value << std::endl;
 	return (0);
 }
