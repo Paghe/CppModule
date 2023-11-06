c6adb7ba14badea3b273dea150600fb3493613ba
Alessandro Paghera <apaghera@2-I-5.42heilbronn.de>
2023-11-06 13:55:51 +0100

    forgot to free memory

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,12 +1,19 @@
 #include "Base.hpp"
 
+void	leaks(void)
+{
+	system("leaks Base");
+}
+
 int	main (void)
 {
+//	atexit(leaks);
 	Base test;
 	Base *check;
 	check = test.generate();
 	Base &ref = *check;
 	test.identify(check);
 	test.identify(ref);
+	delete check;
 	return (0);
 }
