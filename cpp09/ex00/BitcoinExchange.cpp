b51ea985be8f0ad163541b9f6c1504a44211315e
Alessandro Paghera <apaghera@1-E-23.42heilbronn.de>
2023-11-21 13:59:10 +0100

    done with ex00

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -20,13 +20,15 @@ void Bitcoin::insertElement(std::string key, double value)
 
 double Bitcoin::getDataValue(std::string key)
 {
+
 	std::map<std::string, double>::iterator it = this->find(key);
-	if (it != end())
-		return (it->second);
+	if (it != this->end())
+		return 	(it->second);
 	else
 	{
-		std::map<std::string, double>::iterator it = this->lower_bound(key);
-		return (it->second);
+		it = this->lower_bound(key);
+		if (it != this->end())
+			return (it->second);
 	}
 	return (-1);
 }
@@ -64,11 +66,35 @@ int	splitStr(std::string str, char delimiter)
 	while (getline(split, splittedValueFirst, delimiter) &&
 			getline(split, splittedValueSecond, delimiter) &&
 			getline(split, splittedValueThird, delimiter))
-	if (splittedValueSecond[0] >= '4' || (splittedValueSecond[0] == '3' && splittedValueSecond[0] >= '2'))
-		return (-1);
-	else if (splittedValueThird[0] >= '2' || (splittedValueSecond[0] == '1' && splittedValueSecond[0] >= '3'))
-		return (-1);
+	{
+		if (splittedValueSecond[0] >= '4' ||
+			(splittedValueSecond[0] == '3' && splittedValueSecond[0] >= '2'))
+			return (-1);
+		else if (splittedValueThird[0] >= '2' ||
+				 (splittedValueSecond[0] == '1' && splittedValueSecond[0] >= '3'))
+			return (-1);
+	}
 	return (0);
+}
+
+double errorValue(double value)
+{
+	if (value < 0)
+	{
+		std::cerr << "Negative number are not allowed\n";
+		return (1);
+	}
+	else if (value > 1000)
+	{
+		std::cerr << "Error: too large a number\n";
+		return (1);
+	}
+	return (0);
+}
+
+void printMultiplied(std::string date, double dataValue, double value)
+{
+	std::cout << date <<  " => " << value << " = " << dataValue * value << std::endl;
 }
 
 void Bitcoin::calcValueMultiplied(const std::string valueFile)
@@ -88,35 +114,20 @@ void Bitcoin::calcValueMultiplied(const std::string valueFile)
 		if (getline(ss, date, '|') &&
 			getline(ss, str, '|'))
 		{
+			date = date.substr(0, date.size() - 1);
 			value = atof(str.c_str());
-			if (value < 0)
-			{
-				std::cerr << "Negative number are not allowed\n";
-				continue ;
-			}
-			else if (value > 1000)
-			{
-				std::cerr << "Error: too large a number\n";
-				continue ;
-			}
-			if (splitStr(date, '-') == -1)
-			{
-				std::cout << "Wrond Date: " << date << std::endl;
-				continue ;
-			}
 			dataValue = getDataValue(date);
-			if (dataValue == -1)
-			{
-				std::cout << "No data has been found on the database" << std::endl;
+			if (errorValue(value) == 1)
 				continue ;
-			}
-			std::cout << "VALUE MULTIPLIED " << value * dataValue << std::endl;
+			else if (splitStr(date, '-') == -1)
+				std::cout << "Wrong Date: " << date << std::endl;
+			else if (dataValue == -1)
+				std::cout << "No data has been found on the database" << std::endl;
+			else
+				printMultiplied(date, dataValue, value);
 		}
 		else
-		{
 			std::cout << "Invalid format" << std::endl;
-			return ;
-		}
 	}
 }
 
